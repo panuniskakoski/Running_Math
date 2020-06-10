a544c0eb759f623274ce045d02b0ade4b811aec4
Panu Niskakoski <46327100+panuniskakoski@users.noreply.github.com>
2020-06-10 18:53:08 +0300

    Iteration 1
    - First level
    - Player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+    public Animator animator;
+
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool interaction = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        interaction = false;
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButton("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+    }
+
+    
+    void FixedUpdate() {
+        // Move the character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+        interaction = false;
+
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+}
